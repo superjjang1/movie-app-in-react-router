b31750d666a02f901ad3e066db9bb50a3703768f
Sean Yim <superjjang89@gmail.com>
2019-09-11 15:00:05 -0400

    added fontawesome package, edited singlemovie.js

--- src/components/SingleMovie.js
+++ src/components/SingleMovie.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import axios from 'axios';
+import config from '../config';
 
 
 class SingleMovie extends Component{
@@ -11,13 +12,25 @@ class SingleMovie extends Component{
     }
     async componentDidMount(){
         const movieId = this.props.match.params.movieId;
-        const resp = await axios.get()
-        
+        const thisMovieDetail = `https://api.themoviedb.org/3/movie/${movieId}?api_key=${config.api_key}`;
+        const resp = await axios.get(thisMovieDetail)
+        console.log(resp.data);
+        this.setState({
+            movieData: resp.data
+        })
     }
     render(){
+        if(this.state.movieData.id === undefined){
+            return(<h1>loading....</h1>)
+        }
+
         
         return(
-            <h1>hi</h1>
+            <div className="container"> 
+            <img src={`http://image.tmdb.org/t/p/w300${this.state.movieData.production_companies[0].logo_path}`} alt={this.state.movieData.title} />
+            <h1>{this.state.movieData.title}</h1>
+            <h1>{this.state.movieData.tagline}</h1>
+            </div>
         )
     }
 }
---
+++ src/components/Spinner.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSpinner } from '@fortawesome/free-solid-svg-icons';
+import { library } from '@fortawesome/fontawesome-svg-core';
+library.add(faSpinner);
